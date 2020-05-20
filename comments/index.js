78a56ed3e6929f49bc05e1a60189f5cbf3b2a5c5
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-05-20 11:38:36 -0400

    Set up express routes for comments

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+const allComments = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(allComments[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = allComments[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  allComments[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on port 4001...');
+});
